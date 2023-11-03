f8a21af530894d38a5bcc48adfe29c4841eb2418
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2023-11-03 16:24:11 -0700

    Average last 10 samples for vision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Vision.java
@@ -7,8 +7,13 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 @Config
 public class Vision {
+    public static Queue<Integer> values = new LinkedList<>();
+    public static int SAMPLE_COUNT = 10;
     private DistanceSensor leftSensor;
     private DistanceSensor middleSensor;
     private DistanceSensor rightSensor;
@@ -19,10 +24,7 @@ public class Vision {
         rightSensor = hardwareMap.get(DistanceSensor.class, "rightDistanceSensor");
     }
 
-    // 0 = left
-    // 1 = middle
-    // 2 = right
-    public int objectPosition() {
+    public int rawObjectPosition() {
         double lDistance = leftSensor.getDistance(DistanceUnit.INCH);
         double mDistance = middleSensor.getDistance(DistanceUnit.INCH);
         double rDistance = rightSensor.getDistance(DistanceUnit.INCH);
@@ -44,6 +46,22 @@ public class Vision {
         else {
             return 0;
         }
+    }
+
+    // 0 = left
+    // 1 = middle
+    // 2 = right
+    public int objectPosition() {
+        int pos = rawObjectPosition();
+        values.add(pos);
+        if (values.size() > SAMPLE_COUNT) {
+            values.remove();
+        }
+        double total = 0;
+        for (int val : values) {
+            total += val;
+        }
+        return (int) Math.round(total / SAMPLE_COUNT);
     }
 
     public void displayTelemetry(Telemetry telemetry) {
