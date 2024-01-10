808cc46b239626b35f107858803d3b355bd82dfe
precc <cumulus117@gmail.com>
2024-01-09 16:38:36 -0800

    integrate camera processor with driver station

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/CameraProcessorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/CameraProcessorTest.java
@@ -15,33 +15,49 @@ import org.opencv.imgproc.Imgproc;
 @Autonomous
 // This uses the CameraProcessor
 public class CameraProcessorTest extends LinearOpMode {
+    public CameraProcessor.DebugMode debugMode = CameraProcessor.DebugMode.DriverStation;
     public void runOpMode() throws InterruptedException {
-        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        if (debugMode == CameraProcessor.DebugMode.Dashboard) {
+            telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        }
+
         // Create an instance of the camera processor
         final CameraProcessor processor = new CameraProcessor();
+        processor.setDebugMode(debugMode);
 
         int w = 1920;
         int h = 1080;
 
-        new VisionPortal.Builder()
+        VisionPortal portal = new VisionPortal.Builder()
                 // Get the actual camera on the robot, add the processor, state the orientation of the camera.
                 .setCamera(hardwareMap.get(WebcamName.class, "webcam"))
                 .setCameraResolution(new Size(w, h))
                 .addProcessor(processor)
                 .setCamera(BuiltinCameraDirection.BACK)
                 .build();
+
         // Activate Bitmap consumers to ACTUALLY show the image on Dashboard. If the "max frames per second" variable is set to 0, there isn't a max limit and the camera can take infinite photos.
-        FtcDashboard.getInstance().startCameraStream(processor, 0);
+        if (debugMode == CameraProcessor.DebugMode.Dashboard) {
+            FtcDashboard.getInstance().startCameraStream(processor, 0);
+        }
 
-        waitForStart();
-
-        while (opModeIsActive()) {
+        while (opModeInInit()) {
+            // the camera processor got result only when it is in active
             telemetry.addData("Position:", processor.position);
             telemetry.addData("Left Rectangle Saturation:", processor.getSatRectLeft());
             telemetry.addData("Center Rectangle Saturation:", processor.getSatRectCenter());
             telemetry.addData("Right Rectangle Saturation:", processor.getSatRectRight());
             telemetry.update();
+
             sleep(100);
+        }
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // the camera processor got result only when it is in active
+            telemetry.addData("loop:", "loop");
+            telemetry.update();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CameraProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CameraProcessor.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -22,15 +24,23 @@ import org.opencv.imgproc.Imgproc;
 import java.util.concurrent.atomic.AtomicReference;
 @Config
 public class CameraProcessor implements VisionProcessor, CameraStreamSource {
+    public enum DebugMode {
+        None,
+        DriverStation,
+        Dashboard
+    }
+
     // State the dimensions of the rectangles for the 3 locations of the team prop.
     public static Rect leftRect = new Rect(0, 750, 200 , 280);
     public static Rect centerRect = new Rect(550, 750, 300, 210);
     public static Rect rightRect = new Rect(1050, 780, 400, 250);
-    public String position;
+    public int position;
     private double satRectLeft;
     private double satRectCenter;
     private double satRectRight;
     Mat hsvMat = new Mat();
+
+    private DebugMode debugMode = DebugMode.None;
 
     /* Create an AtomicReference that holds a Bitmap that is called "lastFrame" and give it some random dimensions.
      * The "AtomicReference" means that it is thread safe.
@@ -45,32 +55,35 @@ public class CameraProcessor implements VisionProcessor, CameraStreamSource {
     private AtomicReference<Bitmap> lastFrameWithBoundingRect =
             new AtomicReference<>(Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565));
 
-
     public void init(int width, int height, CameraCalibration calibration) {
         // Do nothing. Since I implemented some classes in this class I need to implement their methods. Unfortunately I don't need them.
     };
 
     public Object processFrame(Mat frame, long captureTimeNanos) {
-        // Create a Bitmap that is called "b" and give it the dimensions of the frame that we input to the function.
-        Bitmap b = Bitmap.createBitmap(frame.width(), frame.height(), Bitmap.Config.RGB_565);
-        // Convert the frame to the Bitmap "b."
-        Utils.matToBitmap(frame, b);
-        // Set the "lastFrame" to the Bitmap "b." So that AtomicReference container ACTUALLY holds the Bitmap.
-        lastFrame.set(b);
-        // Make a color "green."
-        Scalar green = new Scalar(0, 255, 0);
-        // Clone the Matrix. The original matrix is for the processing purposes and the cloned matrix is for the display purposes.
-        Mat frameWithBoundingRect = frame.clone();
-        // Draw rectangles on the cloned matrix.
-        Imgproc.rectangle(frameWithBoundingRect, new Point(leftRect.x, leftRect.y), new Point(leftRect.x + leftRect.width, leftRect.y + leftRect.height), green, 4);
-        Imgproc.rectangle(frameWithBoundingRect, new Point(centerRect.x, centerRect.y), new Point(centerRect.x + centerRect.width, centerRect.y + centerRect.height), green, 4);
-        Imgproc.rectangle(frameWithBoundingRect, new Point(rightRect.x, rightRect.y), new Point(rightRect.x + rightRect.width, rightRect.y + rightRect.height), green, 4);
-        // Create a Bitmap "bb" that is the same size as "frameWithBoundingRect."
-        Bitmap bb = Bitmap.createBitmap(frameWithBoundingRect.width(), frameWithBoundingRect.height(), Bitmap.Config.RGB_565);
-        // Convert the cloned matrix to the Bitmap "bb."
-        Utils.matToBitmap(frameWithBoundingRect, bb);
-        // Set the "lastFrameWithBoundingRect" to the Bitmap "bb." So that AtomicReference container ACTUALLY holds the Bitmap.
-        lastFrameWithBoundingRect.set(bb);
+        if (debugMode == DebugMode.Dashboard) {
+            // Create a Bitmap that is called "b" and give it the dimensions of the frame that we input to the function.
+            Bitmap b = Bitmap.createBitmap(frame.width(), frame.height(), Bitmap.Config.RGB_565);
+            // Convert the frame to the Bitmap "b."
+            Utils.matToBitmap(frame, b);
+            // Set the "lastFrame" to the Bitmap "b." So that AtomicReference container ACTUALLY holds the Bitmap.
+            lastFrame.set(b);
+
+            // Make a color "green."
+            Scalar green = new Scalar(0, 255, 0);
+            // Clone the Matrix. The original matrix is for the processing purposes and the cloned matrix is for the display purposes.
+            Mat frameWithBoundingRect = frame.clone();
+            // Draw rectangles on the cloned matrix.
+            Imgproc.rectangle(frameWithBoundingRect, new Point(leftRect.x, leftRect.y), new Point(leftRect.x + leftRect.width, leftRect.y + leftRect.height), green, 4);
+            Imgproc.rectangle(frameWithBoundingRect, new Point(centerRect.x, centerRect.y), new Point(centerRect.x + centerRect.width, centerRect.y + centerRect.height), green, 4);
+            Imgproc.rectangle(frameWithBoundingRect, new Point(rightRect.x, rightRect.y), new Point(rightRect.x + rightRect.width, rightRect.y + rightRect.height), green, 4);
+            // Create a Bitmap "bb" that is the same size as "frameWithBoundingRect."
+            Bitmap bb = Bitmap.createBitmap(frameWithBoundingRect.width(), frameWithBoundingRect.height(), Bitmap.Config.RGB_565);
+            // Convert the cloned matrix to the Bitmap "bb."
+            Utils.matToBitmap(frameWithBoundingRect, bb);
+            // Set the "lastFrameWithBoundingRect" to the Bitmap "bb." So that AtomicReference container ACTUALLY holds the Bitmap.
+            lastFrameWithBoundingRect.set(bb);
+        }
+
         //-----
         Imgproc.cvtColor(frame, hsvMat, Imgproc.COLOR_RGB2HSV);
 
@@ -79,20 +92,41 @@ public class CameraProcessor implements VisionProcessor, CameraStreamSource {
         satRectRight = getAvgSaturation(hsvMat, rightRect);
 
         if (satRectLeft > satRectCenter && satRectLeft > satRectRight) {
-            position = "left";
+            // position = "left";
+            position = 2;
+        } else if (satRectCenter > satRectRight) {
+            // position = "center";
+            position = 1;
+        } else {
+            // position = "right";
+            position = 0;
         }
-        else if (satRectCenter > satRectRight) {
-            position = "center";
-        }
-        else {
-            position = "right";
-        }
+
         return position;
     };
 
-    private double getAvgSaturation(Mat input, Rect rect) {
-        Mat subMat = input.submat(rect);
-        return Core.mean(subMat).val[1];
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+        if (debugMode == DebugMode.DriverStation) {
+            Paint paint=new Paint();
+            paint.setColor(Color.GREEN);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(scaleCanvasDensity * 4);
+
+            android.graphics.Rect left=makeGraphicsRect(leftRect, scaleBmpPxToCanvasPx);
+            android.graphics.Rect center=makeGraphicsRect(centerRect, scaleBmpPxToCanvasPx);
+            android.graphics.Rect right=makeGraphicsRect(rightRect, scaleBmpPxToCanvasPx);
+
+            canvas.drawRect(left, paint);
+            canvas.drawRect(center, paint);
+            canvas.drawRect(right, paint);
+        }
+    };
+
+    // This lets Dashboard get all of our Bitmaps!!!
+    public void getFrameBitmap(Continuation<? extends Consumer<Bitmap>> continuation) {
+        if (debugMode == DebugMode.Dashboard) {
+            continuation.dispatch(bitmapConsumer -> bitmapConsumer.accept(lastFrameWithBoundingRect.get()));
+        }
     }
 
     public double getSatRectLeft() {
@@ -107,13 +141,21 @@ public class CameraProcessor implements VisionProcessor, CameraStreamSource {
         return satRectRight;
     }
 
-    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
-        // do nothing. This method was just brought up because I used "implements VisionProcessor" in the class and Java forced me to implement it. :(.
-    };
+    public void setDebugMode(DebugMode debugMode) {
+        this.debugMode = debugMode;
+    }
 
-    // This lets Dashboard get all of our Bitmaps!!!
-    public void getFrameBitmap(Continuation<? extends Consumer<Bitmap>> continuation) {
-        // continuation.dispatch(bitmapConsumer -> bitmapConsumer.accept(lastFrame.get()));
-        continuation.dispatch(bitmapConsumer -> bitmapConsumer.accept(lastFrameWithBoundingRect.get()));
+    private double getAvgSaturation(Mat input, Rect rect) {
+        Mat subMat = input.submat(rect);
+        return Core.mean(subMat).val[1];
+    }
+
+    private android.graphics.Rect makeGraphicsRect(Rect rect, float scaleBmpPxToCanvasPx){
+        int left=Math.round(rect.x * scaleBmpPxToCanvasPx);
+        int top=Math.round(rect.y * scaleBmpPxToCanvasPx);
+        int right=left+Math.round(rect.width * scaleBmpPxToCanvasPx);
+        int bottom=top+Math.round(rect.height * scaleBmpPxToCanvasPx);
+
+        return new android.graphics.Rect(left,top,right,bottom);
     }
 }
