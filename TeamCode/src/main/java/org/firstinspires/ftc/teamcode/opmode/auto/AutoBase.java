dcc5cff1be38c5dd94858120524bde8fabd28014
precc <cumulus117@gmail.com>
2024-01-09 19:49:20 -0800

    integrate camera vision for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoBase.java
@@ -1,18 +1,24 @@
 package org.firstinspires.ftc.teamcode.opmode.auto;
 
 import android.util.Log;
+import android.util.Size;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.CameraProcessor;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Memory;
 import org.firstinspires.ftc.teamcode.subsystems.Outtake;
 import org.firstinspires.ftc.teamcode.subsystems.Plane;
 import org.firstinspires.ftc.teamcode.subsystems.Vision;
 import org.firstinspires.ftc.teamcode.util.AutoActionScheduler;
+import org.firstinspires.ftc.vision.VisionPortal;
 
 @Config
 public abstract class AutoBase extends LinearOpMode {
@@ -22,6 +28,12 @@ public abstract class AutoBase extends LinearOpMode {
     protected Vision vision;
     protected Plane plane;
     protected AutoActionScheduler sched;
+
+    protected CameraProcessor processor;
+    protected VisionPortal portal;
+
+    protected int w = 1920;
+    protected int h = 1080;
 
     public static int SPIKE = 0;
 
@@ -42,9 +54,17 @@ public abstract class AutoBase extends LinearOpMode {
         this.drive = new MecanumDrive(hardwareMap, Memory.LAST_POSE);
         this.intake = new Intake(hardwareMap);
         this.outtake = new Outtake(hardwareMap);
-        this.vision = new Vision(hardwareMap);
+        // this.vision = new Vision(hardwareMap);
         this.plane = new Plane(hardwareMap);
         this.sched = new AutoActionScheduler(this::update);
+        this.processor = new CameraProcessor();
+        this.portal = new VisionPortal.Builder()
+                // Get the actual camera on the robot, add the processor, state the orientation of the camera.
+                .setCamera(hardwareMap.get(WebcamName.class, "webcam"))
+                .setCameraResolution(new Size(w, h))
+                .addProcessor(processor)
+                .setCamera(BuiltinCameraDirection.BACK)
+                .build();
 
         outtake.resetMotors();
 
@@ -55,8 +75,17 @@ public abstract class AutoBase extends LinearOpMode {
         onInit();
 
         while (opModeInInit()) {
-            SPIKE = vision.objectPosition();
-            vision.displayTelemetry(telemetry);
+            telemetry.addData("camera check", "working");
+            telemetry.addData("Position:", processor.position);
+            telemetry.addData("Left Rectangle Saturation:", processor.getSatRectLeft());
+            telemetry.addData("Center Rectangle Saturation:", processor.getSatRectCenter());
+            telemetry.addData("Right Rectangle Saturation:", processor.getSatRectRight());
+            telemetry.update();
+            // Below is the code for the distance sensors
+            // SPIKE = vision.objectPosition();
+            // Below is the code for the camera vision
+            SPIKE = processor.position;
+            //vision.displayTelemetry(telemetry);
 
             printDescription();
 
