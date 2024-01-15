9d7cb5235631050a752d4c623a1a2547f1260817
Nv7-Github <junk4Nv7@gmail.com>
2024-01-14 19:58:30 -0800

    Changes during competition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AutoConstants.java
@@ -6,9 +6,9 @@ import com.acmerobotics.roadrunner.Pose2d;
 @Config
 public class AutoConstants {
    public static Pose2d[] blueScoring = {
-           new Pose2d(42, 30, Math.toRadians(180)),
-           new Pose2d(42, 36, Math.toRadians(180)),
-           new Pose2d(42, 40, Math.toRadians(180))
+           new Pose2d(43, 30, Math.toRadians(180)),
+           new Pose2d(43, 36, Math.toRadians(180)),
+           new Pose2d(43, 40, Math.toRadians(180))
    };
    public static Pose2d[] redScoring = {
            new Pose2d(43, -40, Math.toRadians(180)),
