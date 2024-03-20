4d46ec4a5474f123b994cd3ab7d3950baae0862d
Nv7-Github <junk4Nv7@gmail.com>
2024-03-19 21:38:55 -0700

    Driver 2 tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/ManualDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/ManualDrive.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
+import static org.firstinspires.ftc.teamcode.util.control.PIDFControllerKt.EPSILON;
+
 import android.util.Log;
 
 import com.acmerobotics.dashboard.config.Config;
@@ -24,6 +26,8 @@ import org.firstinspires.ftc.teamcode.util.ActionScheduler;
 import org.firstinspires.ftc.teamcode.util.GamePadController;
 import org.firstinspires.ftc.teamcode.util.LED;
 import org.firstinspires.ftc.teamcode.util.SmartGameTimer;
+import org.firstinspires.ftc.teamcode.util.control.PIDCoefficients;
+import org.firstinspires.ftc.teamcode.util.control.PIDFController;
 
 @Config
 @TeleOp(group = "Drive")
@@ -31,7 +35,7 @@ public class ManualDrive extends LinearOpMode {
    public static double TURN_SPEED = 0.75;
    public static double DRIVE_SPEED = 1;
    public static double SLOW_TURN_SPEED = 0.3;
-   public static double D2_SLOW_TURN = 0.4;
+   public static double D2_SLOW_TURN = 0.25;
    public static double SLOW_DRIVE_SPEED = 0.3;
    public static double VISION_RANGE = 20;
    public static double VISION_CLOSE_DIST = 5;
@@ -44,6 +48,9 @@ public class ManualDrive extends LinearOpMode {
    private Outtake outtake;
    private Plane plane;
    private LED led;
+
+   public static PIDCoefficients HEADING_PID = new PIDCoefficients(2, 0, 0);
+   private PIDFController headingPid;
 
    @Override
    public void runOpMode() throws InterruptedException {
@@ -61,6 +68,7 @@ public class ManualDrive extends LinearOpMode {
       outtake = new Outtake(hardwareMap);
       plane = new Plane(hardwareMap);
       led = new LED(hardwareMap);
+      headingPid = new PIDFController(HEADING_PID);
 
       if (Memory.RAN_AUTO) {
          smartGameTimer = new SmartGameTimer(true);
@@ -119,6 +127,7 @@ public class ManualDrive extends LinearOpMode {
       Memory.LAST_POSE = drive.pose;
    }
 
+   private double prevInputX = 0;
    private void move() {
       // Main driver controls
       double visionDist = 10000;
@@ -138,16 +147,16 @@ public class ManualDrive extends LinearOpMode {
 
       double input_x;
       double input_y;
-      if (-g1.left_stick_y > 0 && intake.isIntaking()) {
+      /*if (-g1.left_stick_y > 0 && intake.isIntaking()) {
          input_x = Math.pow(-g1.left_stick_y * (intake.isIntakeNearWall() ? Range.clip((intake.getDistance()-200)/800, 0.4, 0.8) : 1), 3) * speed;
          input_x = Range.clip(input_x, 0.15, 1);
-         if (intake.willIntakeHitWall()) {
+        if (intake.willIntakeHitWall()) {
             input_x = 0;
-         }
+         } // Messed up when intaking in corner
       } else {
          input_x = Math.pow(-g1.left_stick_y, 3) * speed;
-      }
-
+      }*/
+      input_x = Math.pow(-g1.left_stick_y, 3) * speed;
       input_y = Math.pow(-g1.left_stick_x, 3) * speed;
 
       Vector2d input = new Vector2d(input_x, input_y);
@@ -158,9 +167,24 @@ public class ManualDrive extends LinearOpMode {
       if (g1.rightBumper()) input_turn -= SLOW_TURN_SPEED;
 
       // Driver 2 slow strafe
-      input = input.plus(new Vector2d(g2.left_stick_y * SLOW_DRIVE_SPEED, g2.left_stick_x * SLOW_DRIVE_SPEED * 1.5));
+      input = input.plus(new Vector2d(g2.left_stick_y * SLOW_DRIVE_SPEED, g2.left_stick_x * SLOW_DRIVE_SPEED));
       input_turn += g2.left_trigger * D2_SLOW_TURN;
       input_turn -= g2.right_trigger * D2_SLOW_TURN;
+
+      if (Math.abs(g2.left_stick_x) != 0 && Math.abs(prevInputX) < EPSILON) {
+         headingPid.setTargetPosition(drive.pose.heading.toDouble());
+         Log.d("TARGET", "target");
+      }
+      prevInputX = g2.left_stick_x;
+      if (Math.abs(g1.left_stick_x + g1.left_stick_y + input_turn) < EPSILON && Math.abs(g2.left_stick_x) > 0) { // Do heading lock
+         input_turn = headingPid.update(drive.pose.heading.toDouble());
+         if (g2.left_stick_x > 0) { // Account for heading turn overpowering strafe
+            input = input.plus(new Vector2d(0, Math.abs(input_turn)));
+         } else {
+            input = input.plus(new Vector2d(0, -Math.abs(input_turn)));
+         }
+         Log.d("TARGET", String.valueOf(input_turn));
+      }
 
       drive.setDrivePowers(new PoseVelocity2d(input, input_turn));
    }
@@ -245,6 +269,9 @@ public class ManualDrive extends LinearOpMode {
       // Outtake controls
       if (g2.yOnce()) {
          if (!outtake.isArmScoring()) {
+            if (intake.pixelCount == 1) {
+               sched.queueAction(new SequentialAction(outtake.extendOuttakeBarelyOut(), new SleepAction(0.3)));
+            }
             sched.queueAction(intake.intakeOff());
             sched.queueAction(new SequentialAction(
                     intake.pixelCount == 1 ? outtake.clawSingleClosed() : outtake.clawClosed(),
@@ -259,6 +286,9 @@ public class ManualDrive extends LinearOpMode {
       }
       if (g2.xOnce()) {
          if (!outtake.isArmScoring()) {
+            if (intake.pixelCount == 1) {
+               sched.queueAction(new SequentialAction(outtake.extendOuttakeBarelyOut(), new SleepAction(0.3)));
+            }
             sched.queueAction(intake.intakeOff());
             sched.queueAction(new SequentialAction(
                     intake.pixelCount == 1 ? outtake.clawSingleClosed() : outtake.clawClosed(),
