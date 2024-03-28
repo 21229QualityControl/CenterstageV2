dbd9661a0f408cc87714d52661535d7face5ab5b
precc <cumulus117@gmail.com>
2024-03-27 22:24:47 -0700

    Add Plane

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Plane.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Plane.java
@@ -12,31 +12,31 @@ import org.firstinspires.ftc.teamcode.util.HardwareCreator;
 
 @Config
 public class Plane {
-   public static double LATCH_SCORED = 0.36;
-   public static double LATCH_CLOSED = 0.56;
-   public static double WRIST_STORED = 0.47;
-   public static double WRIST_SCORED = 0.34;
+   public static double LATCH_SCORED = 0.32;
+   public static double LATCH_CLOSED = 0.36;
+//   public static double WRIST_STORED = 0.47;
+//   public static double WRIST_SCORED = 0.34;
 
    final Servo latch;
-   final Servo wrist;
+//   final Servo wrist;
 
    public Plane(HardwareMap hardwareMap) {
       this.latch = HardwareCreator.createServo(hardwareMap, "planeLatch");
-      this.wrist = HardwareCreator.createServo(hardwareMap, "planeWrist");
+//      this.wrist = HardwareCreator.createServo(hardwareMap, "planeWrist");
    }
 
    public void initialize() {
       latch.setPosition(LATCH_CLOSED);
-      wrist.setPosition(WRIST_STORED);
+//      wrist.setPosition(WRIST_STORED);
    }
 
    public Action scorePlane() {
       return new SequentialAction(
-              new ActionUtil.ServoPositionAction(wrist, WRIST_SCORED),
-              new SleepAction(0.4),
+//              new ActionUtil.ServoPositionAction(wrist, WRIST_SCORED),
+//              new SleepAction(0.4),
               new ActionUtil.ServoPositionAction(latch, LATCH_SCORED),
               new SleepAction(0.3),
-              new ActionUtil.ServoPositionAction(wrist, WRIST_STORED),
+//              new ActionUtil.ServoPositionAction(wrist, WRIST_STORED),
               new ActionUtil.ServoPositionAction(latch, LATCH_CLOSED)
       );
    }
